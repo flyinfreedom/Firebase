12258ae4df28631889c3d85dc11218e2d3777c86
flyinfreedom <flyinfreedom1124@gmail.com>
2020-04-04 00:14:41 +0800

    firebase first commit

---
+++ firebase/src/firebase/credentials.js
@@ -0,0 +1,12 @@
+export default {
+    config: {
+        apiKey: "AIzaSyBPONg848yq0aP65KrG8_nb4mZYVaW-ZZs",
+        authDomain: "fir-lib-7e89c.firebaseapp.com",
+        databaseURL: "https://fir-lib-7e89c.firebaseio.com",
+        projectId: "fir-lib-7e89c",
+        storageBucket: "fir-lib-7e89c.appspot.com",
+        messagingSenderId: "280107230314",
+        appId: "1:280107230314:web:ef803bdcad93b5e9ab5613",
+        measurementId: "G-7NJH01S956"
+    }
+}
